6c2a1ac97bdba8361d7eebbc591baec1ce67c217
alenac27 <124631237+alenac27@users.noreply.github.com>
2023-12-21 20:21:04 +0100

    added basic form handling and added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generatePlaylist(event) {
+  event.preventDefault();
+  new Typewriter("#playlist", {
+    strings: "Here is a playlist",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
 
+let playlistFormElement = document.querySelector("#playlist-generator-form");
+playlistFormElement.addEventListener("submit", generatePlaylist);
