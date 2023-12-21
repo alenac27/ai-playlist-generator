933d7f6a75d3a303b379a4b4ec05f070a80d8251
alenac27 <124631237+alenac27@users.noreply.github.com>
2023-12-21 21:25:18 +0100

    integrated AI API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
-function generatePlaylist(event) {
-  event.preventDefault();
+function displayPlaylist(response) {
   new Typewriter("#playlist", {
-    strings: "Here is a playlist",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePlaylist(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+
+  let apiKey = "d2b000c63899bdd9d62748ft3o63f2a2";
+  let prompt = `User instructions: Generate a playlist in genre ${instructionsInput.value}`;
+  let context =
+    "You are a music expert with a deep knowledge of all music genres worldwide. Your mission is to generate a playlist of 10 songs as a list element in basic HTML without a title. Do not display list-style-type: disc, please display an emoji which fits to the name of the singer or the title of the song. Please follow the user instructions.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPlaylist);
 }
 
 let playlistFormElement = document.querySelector("#playlist-generator-form");
