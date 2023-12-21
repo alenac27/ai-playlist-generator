133605a40a1c9ad095ac3a0ba7bc274915100444
alenac27 <124631237+alenac27@users.noreply.github.com>
2023-12-21 22:23:17 +0100

    improved UX

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ function generatePlaylist(event) {
     "You are a music expert with a deep knowledge of all music genres worldwide. Your mission is to generate a playlist of 10 songs as a list element in basic HTML without a title. Do not display list-style-type: disc, please display an emoji which fits to the name of the singer or the title of the song. Please follow the user instructions.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let playlistElement = document.querySelector("#playlist");
+  playlistElement.classList.remove("hidden");
+  playlistElement.innerHTML = "Generating your playlist  🎵  🎼 ❤️ ";
+
   axios.get(apiUrl).then(displayPlaylist);
 }
 
